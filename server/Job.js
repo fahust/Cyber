14ec13c276b5198bf3445dbfea6ef4b827b20247
kevin <k.dellova@coccinet.com>
2020-08-05 15:01:16 +0200

    react started and socket on node, with all job primary

--- server/Job.js
+++ server/Job.js
@@ -27,6 +27,7 @@ function randomIntFromInterval(min, max) { // min and max included
                     byUser : data.user.username,
                     type : data.type,
                 }
+                this.users[data.cible.username].socket.emit('add interuptable', { data: this.users[data.cible.username].data.interuptable });
                 return true
             }else{
                 return false
@@ -47,26 +48,26 @@ function randomIntFromInterval(min, max) { // min and max included
     interuptJob(socket,data){
         if(this.users[data.user.username]){
             if(this.users[data.user.username].data.interuptable.byUser !== ''){
-                this.clearInteruptableToCible();
+                this.clearInteruptableToCible(socket,data);
             }
         }
     }
 
     /*
         1 : rediger information
-        2 : pirater information //interuptable
-        3 : pirater monnaie //interuptable
+        2 : pirater information //interuptable //illegal
+        3 : pirater monnaie //interuptable //illegal
         4 : créer un module
         5 : scan d'information (pour trouver un virus)
         6 : implanter un module dans une info
         7 : extraire un module d'une info
         8 : debuguer une information d'un virus
-        9 : implanter virus dans une information
+        9 : implanter virus dans une information //illegal
         10 : debuguer un joueur d'un virus
-        11 : vol de membre
-        12 : vente de membre
-        13 : achat de membre
-        14 : supprimer logs illégaux
+        11 : vol de membre //illegal
+        12 : vente de membre //illegal
+        13 : achat de membre //illegal
+        14 : supprimer logs illégaux //illegal
         15 : detecter des illégalité dans les logs d'un joueur //interuptable
     */ 
     endJob(socket,data){
@@ -75,11 +76,9 @@ function randomIntFromInterval(min, max) { // min and max included
 
             if(data.type === 2 || data.type === 3 || data.type === 14){
                 if(this.users[data.cible.username].data.interuptable.byUser === '')
-                value = false;
-                this.clearInteruptableToCible();
+                    value = false;
+                this.clearInteruptableToCible(socket,data);
             }
-
-            this.addJobExp(socket,data);
 
             if(data.type === 1){
                 value = this.classInformation.addInformation(socket,data);
@@ -118,14 +117,18 @@ function randomIntFromInterval(min, max) { // min and max included
                 value = this.classUser.detectIllegalsLogs(socket,data);
             }
             
-            this.addLog(socket,data,value);
+            var exp = 0;
+            if(value != false)
+                exp = this.addJobExp(socket,data);
+
+            this.users[data.cible.username].socket.emit('end job', { data: this.addLog(socket,data,value,exp) });
             
             return value;
         }
     }
 
     startJob(socket,data){
-        if(this.users[data.user.username]){
+        if(this.users[data.user.username] && this.users[data.user.username].prisonMinute <= Date.now()){
             if(data.type === 2 || data.type === 3 || data.type === 14)
                 return this.addInteruptableToCible(socket,data);
             return true;
@@ -135,16 +138,34 @@ function randomIntFromInterval(min, max) { // min and max included
     }
 
     addJobExp(socket,data){
-        this.users[data.user.username].data.jobExp[data.type] += randomIntFromInterval(1,5)/this.users[data.user.username].data.jobExp[data.type]
+        return this.users[data.user.username].data.jobExp[data.type] += randomIntFromInterval(1,5)/this.users[data.user.username].data.jobExp[data.type]
     }
 
-    addLog(socket,data){
+    addLog(socket,data,value){
         if(this.users[data.user.username]){
-            this.users[data.user.username].data.logs = {
+            var date = Date.now();
+
+            const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: "numeric", minute: "numeric", second: "numeric",  };
+            var dateStringed = date.toLocaleDateString('fr-FR', options);
+
+            this.users[data.user.username].data.logs[date] = {
                 type : data.type,
                 cible : data.cible,
-                module : data.module,
+                user : data.user,
+                value : value,
+                date : dateStringed,
             }
+
+            if(data.user.username != data.cible.username){
+                this.users[data.cible.username].data.logs[date] = {
+                    type : data.type,
+                    cible : data.cible,
+                    user : data.user,
+                    value : value,
+                    date : dateStringed,
+                }
+            }
+            return this.users[data.user.username].data.logs[date];
         }
     }
 
